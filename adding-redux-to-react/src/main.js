8fdd311ba9327a64647f3a2aa019a307bafb0aa1
erosen08 <erosenberg39@gmail.com>
2020-06-25 08:34:52 -0400

    finished article

--- adding-redux-to-react/src/main.js
+++ adding-redux-to-react/src/main.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { Provider } from 'react-redux'
 
-import './main.scss';
-import App from './components/App';
+import GroceryPageContainer from '../containers/GroceryPageContainer'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+const App = props => {
+  return(
+    <Provider store={props.store}>
+      <Router history={browserHistory}>
+        <Route path="/groceries" component={GroceryListContainer} />
+        <Route path="/groceries/:id" component={GroceryShowContainer} />
+      </Router>
+    </Provider>
+  )
+}
